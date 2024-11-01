a6f95061ec7d2bca250c4c70cc221cd2a60e4c9d
kimdongjun <cjfal2@kakao.com>
2024-11-01 20:51:51 +0900

    Title: 동전의 가치의 합, Time: 175ms, Memory: 47MB, Status: Wrong Answer - CodeTree

---
+++ 241101/동전의 가치의 합/sum-of-the-values-of-coins.js
@@ -0,0 +1,22 @@
+const stdin = require('fs').readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = (() => {
+    let line = 0;
+    return () => stdin[line++];
+})();
+
+let N = parseInt(input());
+const coins = [7, 5, 2, 1];
+let pointer = 0;
+let answer = 0;
+
+while (N > 0 || pointer < 4) {
+    const coin = coins[pointer];
+    if (N >= coin) {
+        N -= coin
+        answer += 1
+    }
+    else {
+        pointer++
+    }
+}
+console.log(answer);
